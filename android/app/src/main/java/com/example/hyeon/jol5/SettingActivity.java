9e7e3371b12d6b053b81b39faee325dc5e89c865
Jaeguins <jsu4838@naver.com>
2018-11-02 12:29:35 +0900

    setting activity set(for now)

--- android/app/src/main/java/com/example/hyeon/jol5/SettingActivity.java
+++ android/app/src/main/java/com/example/hyeon/jol5/SettingActivity.java
@@ -8,9 +8,11 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.InputStream;
@@ -19,22 +21,49 @@ import java.net.Socket;
 
 
 public class SettingActivity extends AppCompatActivity {
-    SharedPreferences pref=getSharedPreferences("EES",MODE_PRIVATE);
-    SharedPreferences.Editor editor=pref.edit();
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    SeekBar autoUpdate;
+    TextView autoUpdateView;
+    Button resetButton;
+    public static final int MaxDelay=5000;
+    int nowDelay=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
+        autoUpdate=findViewById(R.id.AutoUpdateTermSeekBar);
+        autoUpdateView=findViewById(R.id.AutoUpdateTermValue);
+        autoUpdate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar,int i, boolean b) {moveSeekBar(i);}
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
     @Override
     public void onPause() {
-
+        editor.putInt("autoUpdate",nowDelay);
+        editor.commit();
         super.onPause();
     }
     @Override
     public void onResume() {
-
         super.onResume();
+        pref=getSharedPreferences("EES",MODE_PRIVATE);
+        editor=pref.edit();
+        nowDelay=pref.getInt("autoUpdate",20);
+        autoUpdate.setProgress(nowDelay);
+    }
+    public void moveSeekBar(int i){
+        nowDelay=i;
+        autoUpdateView.setText(String.format("%.02f",MaxDelay/100f*nowDelay/1000+0.05f));
+    }
+    public void resetDefault(){
+        editor.clear();
+        editor.commit();
+        onResume();
     }
 }
