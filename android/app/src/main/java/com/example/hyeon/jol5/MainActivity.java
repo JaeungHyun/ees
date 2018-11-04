52d2e0b3c72b7034f7609e0cee12e6a8f8c92775
Jaeguins <jsu4838@naver.com>
2018-11-05 03:14:06 +0900

    network flow add duplicate code optimized interface improved using shared preference with inputs

--- android/app/src/main/java/com/example/hyeon/jol5/MainActivity.java
+++ android/app/src/main/java/com/example/hyeon/jol5/MainActivity.java
@@ -1,35 +1,68 @@
 package com.example.hyeon.jol5;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 class Packet{//TODO set network flow to get
-
+    static String diff=",";
+    public float nowTemp=0f,nowHum=0f,nowWater=0f, targetTemp=0f;
+    public int targetTerm=0;
+    public String SendToPacket(String type){
+        switch(type){
+            case "refresh":
+                return type;
+            case "nowTemp":
+                return ""+nowTemp;
+            case "nowHum":
+                return ""+nowHum;
+            case "nowWater":
+                return ""+nowWater;
+            case "targetTerm":
+                return ""+targetTerm;
+            case "targetTemp":
+                return ""+targetTemp;
+            default:
+                return "";
+        }
+    }
+    public void ParseToPacket(String stream){
+        String[] data=stream.split(diff);
+        try {
+            nowTemp = Float.parseFloat(data[0]);
+            nowHum = Float.parseFloat(data[1]);
+            nowWater = Float.parseFloat(data[2]);
+            targetTerm = Integer.parseInt(data[3]);
+            targetTemp = Float.parseFloat(data[4]);
+        }catch(NullPointerException e){
+            e.printStackTrace();
+        }
+    }
 }
 class Sender extends Thread{
     MainActivity act;
+    InetSocketAddress ip=new InetSocketAddress("10.149.154.31",12222);//TODO set proper address
     public boolean active=true;
     public int nowDelay=0;
-    public Queue<Packet> packets=new LinkedList<>();
+    public Queue<String> packets=new LinkedList<>();
     public boolean sent=false,flag=false;
     public Sender(MainActivity act){
         this.act=act;
@@ -48,28 +81,59 @@ class Sender extends Thread{
                     sendToServer();
                     sent = true;
                     flag = false;
+                    act.generateToast(act.getString(R.string.update_success));
                 }
-            } catch (InterruptedException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-    public void addPacket(Packet p){
+    public void addPacket(String p){
         packets.add(p);
     }
-    public void sendToServer(){
-        //TODO sendToServer packets
-        act.generateToast(act.getString(R.string.update_success));
+    public String sendToServer() throws IOException {
+        String ret="";
+        while(!packets.isEmpty()){
+            String stream=packets.poll();
+            Socket sock=new Socket();
+            sock.connect(ip);
+            Scanner in=new Scanner(sock.getInputStream());
+            OutputStream out=sock.getOutputStream();
+            out.write(stream.getBytes());
+            while(in.hasNext()){
+                ret+=in.nextLine()+"\n";
+            }
+            out.close();
+            in.close();
+            sock.close();
+        }
+        Log.d("인풋",ret);
+        return ret;
     }
-
 }
+
 public class MainActivity extends AppCompatActivity {
     SharedPreferences pref;
     public Button refresh,option,tempUp,tempDown,termUp,termDown;
     public EditText tempInput,termInput;
-    public TextView nowTemp,nowWater;
+    public TextView nowTemp,nowWater,nowHum;
     public int maxDelay;
     Sender sender;
+    Packet pack=new Packet();
+    class EditAction implements TextView.OnEditorActionListener {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            switch(v.getId()){
+                case R.id.TargetTemp:
+                    pack.targetTemp=Float.parseFloat(tempInput.getText().toString());
+                    break;
+                case R.id.TargetTerm:
+                    pack.targetTerm=Integer.parseInt(termInput.getText().toString());
+                    break;
+            }
+            return false;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,44 +152,47 @@ public class MainActivity extends AppCompatActivity {
         termInput=findViewById(R.id.TargetTerm);
         nowTemp=findViewById(R.id.NowTemp);
         nowWater=findViewById(R.id.NowLevel);
+        nowHum=findViewById(R.id.NowHum);
         //Event Binding
         refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                refreshClick();
+                refresh();
             }
         });
         option.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                optionClick();
+                toOption();
             }
         });
         tempUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tempUpClick();
+                tempUp();
             }
         });
         tempDown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tempDownClick();
+                tempDown();
             }
         });
         termUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                termUpClick();
+                termUp();
             }
         });
         termDown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                termDownClick();
+                termDown();
             }
         });
-        refreshClick();
+        tempInput.setOnEditorActionListener(new EditAction());
+        termInput.setOnEditorActionListener(new EditAction());
+
     }
     public void onResume() {
         super.onResume();
@@ -133,10 +200,19 @@ public class MainActivity extends AppCompatActivity {
         maxDelay=SettingActivity.MaxDelay/100*pref.getInt("autoUpdate",20)+5;
         sender=new Sender(this);
         sender.start();
+        refresh();
+        pack.targetTemp=pref.getFloat("temp",20f);
+        pack.targetTerm=pref.getInt("term",24);
+        setTempView();
+        setTermView();
     }
 
     public void onPause() {
         sender.active = false;
+        SharedPreferences.Editor editor=pref.edit();
+        editor.putFloat("temp",pack.targetTemp);
+        editor.putInt("term",pack.targetTerm);
+        editor.commit();
         super.onPause();
     }
 
@@ -150,50 +226,55 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void refreshClick(){
-
+    public void refresh(){
+        Log.d("REFRESH","REFRESH");
+        triggerSender("refresh");
+        nowTemp.setText(""+pack.nowTemp);
+        nowWater.setText(""+pack.nowWater);
+        nowHum.setText(""+pack.nowHum);
     }
-    public void optionClick(){
+    public void toOption(){
         Intent intent=new Intent(this,SettingActivity.class);
         startActivity(intent);
     }
-    public void tempUpClick(){
-        String tmpStr=tempInput.getText().toString();
-        if(tmpStr.length()==0) tmpStr="0";
-        double tmp=Double.parseDouble(tmpStr);
-        tmp+=.1;
-        tempInput.setText(String.format("%.1f",tmp));
+    public void setTempView(){
+        String tmpStr=String.format("%.1f",pack.targetTemp);
+        tempInput.setText(tmpStr);
+    }
+    public void setTermView(){
+        String tmpStr=String.format("%d",pack.targetTerm);
+        termInput.setText(tmpStr);
+    }
+    public void sendTemp(){
+        triggerSender("temp:"+tempInput.getText().toString());
+    }
+    public void sendTerm(){
+        triggerSender("term:"+termInput.getText().toString());
+    }
+    public void tempUp(){
+        pack.targetTemp+=.1;
+        setTempView();
+        sendTemp();
+    }
+    public void tempDown(){
+        pack.targetTemp-=.1;
+        setTempView();
+        sendTemp();
+    }
+    public void termUp(){
+        pack.targetTerm+=1;
+        setTermView();
+        sendTerm();
+    }
+    public void termDown(){
+        if(pack.targetTerm>1)pack.targetTerm-=1;
+        setTermView();
+        sendTerm();
+    }
+    public void triggerSender(String data){
+        sender.addPacket(data);
         sender.nowDelay=maxDelay;
         sender.sent=false;
     }
-    public void tempDownClick(){
-        String tmpStr=tempInput.getText().toString();
-        if(tmpStr.length()==0) tmpStr="0";
-        double tmp=Double.parseDouble(tmpStr);
-        tmp-=.1;
-        tempInput.setText(String.format("%.1f",tmp));
-        sender.nowDelay=maxDelay;
-        sender.sent=false;
-    }
-    public void termUpClick(){
-        String tmpStr=termInput.getText().toString();
-        if(tmpStr.length()==0) tmpStr="1";
-        int tmp=Integer.parseInt(tmpStr);
-        tmp+=1;
-        termInput.setText(String.format("%d",tmp));
-        sender.nowDelay=maxDelay;
-        sender.sent=false;
-    }
-    public void termDownClick(){
-        String tmpStr=termInput.getText().toString();
-        if(tmpStr.length()==0) tmpStr="1";
-        int tmp=Integer.parseInt(tmpStr);
-        tmp-=1;
-        if(tmp<1)tmp=1;
-        termInput.setText(String.format("%d",tmp));
-        sender.nowDelay=maxDelay;
-        sender.sent=false;
-    }
-
 
 }
