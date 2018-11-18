0b5962b14d060164082ccff2585f8eda9476f966
Jaeguins <jsu4838@naver.com>
2018-11-19 05:05:57 +0900

    Revert "changed sdk version"
    This reverts commit 75646549374db3a80c33b74410386c8c419515cd.

--- android/app/src/main/java/com/example/hyeon/jol5/MainActivity.java
+++ android/app/src/main/java/com/example/hyeon/jol5/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hyeon.jol5;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
