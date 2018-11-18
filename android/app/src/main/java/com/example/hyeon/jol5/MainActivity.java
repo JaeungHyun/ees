75646549374db3a80c33b74410386c8c419515cd
Jaeguins <jsu4838@naver.com>
2018-11-19 04:44:44 +0900

    changed sdk version

--- android/app/src/main/java/com/example/hyeon/jol5/MainActivity.java
+++ android/app/src/main/java/com/example/hyeon/jol5/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.hyeon.jol5;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
